3de8674cf8846a0c84440075394144201dfb5606
Kanchanop6509650229 <kanchanop.bua@dome.tu.ac.th>
2025-03-28 00:30:01 +0700

    feat: Update Postman tests to use dynamic user ID for profile retrieval

--- user-service/src/main/java/com/restaurant/user/domain/models/Profile.java
+++ user-service/src/main/java/com/restaurant/user/domain/models/Profile.java
@@ -1,7 +1,15 @@
 package com.restaurant.user.domain.models;
 
-import jakarta.persistence.*;
 import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "profiles")
