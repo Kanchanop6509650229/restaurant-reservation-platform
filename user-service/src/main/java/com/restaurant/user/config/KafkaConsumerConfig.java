29d3d2c6bfecafcbbcfb559343b5eeaeda8ef9ad
Kanchanop6509650229 <kanchanop.bua@dome.tu.ac.th>
2025-04-04 21:54:37 +0700

    refactor: Simplify Kafka consumer configuration by removing ErrorHandlingDeserializer and restoring direct deserializer usage

--- user-service/src/main/java/com/restaurant/user/config/KafkaConsumerConfig.java
+++ user-service/src/main/java/com/restaurant/user/config/KafkaConsumerConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import com.restaurant.common.events.restaurant.RestaurantEvent;
@@ -31,23 +30,20 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
-        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.restaurant.common.events");
-
-
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        
         JsonDeserializer<RestaurantEvent> deserializer = new JsonDeserializer<>(RestaurantEvent.class);
         deserializer.addTrustedPackages("com.restaurant.common.events");
         deserializer.setUseTypeMapperForKey(true);
-
+        
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, RestaurantEvent> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, RestaurantEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        ConcurrentKafkaListenerContainerFactory<String, RestaurantEvent> factory = 
+                new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
